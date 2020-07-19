b3e7919dad38fe7cbe4675de036784075b423b51
= <krzysztof.surowiec@hotmail.com>
2020-07-19 16:54:19 +0200

    Adding auth blueprint view functions and templates

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -27,6 +27,9 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
     
+    from . import auth
+    app.register_blueprint(auth.bp)
+    
     # A simple page that says hello
     @app.route('/hello')
     def hello():
